c005c6968b57268cd520d1d3c5a07fc0e7800700
Ivan Daniel Gutierrez Mendoza <igutierrez@agarcia.com.mx>
2018-01-17 10:10:45 -0600

    Se cambia URL de condifuracion

--- app/admin/application/config/config.php
+++ app/admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://104.131.42.154/admin/';
+$config['base_url'] = 'http://104.236.191.171:8080/admin/';
 
 /*
 |--------------------------------------------------------------------------
